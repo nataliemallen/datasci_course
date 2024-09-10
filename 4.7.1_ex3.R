1f8e85911ea9b949b35c6d9bcd20de260c193327
nataliemallen <nataliemarionallen@gmail.com>
2024-09-10 14:47:27 -0400

    updated 4.7.1_ex3.R

--- 4.7.1_ex3.R
+++ 4.7.1_ex3.R
@@ -1,53 +1,73 @@
+# Header ------------------------------------------------------------------
+# Assignment name: Assignment3_RDataFram
+# Author name: Natalie Allen
+# Date: September 10, 2024
+# Notes:
+
+# File setup --------------------------------------------------------------
+
+# set working directory below
 setwd("/Users/natal/Documents/Purdue/Data_science_biol/DataSceinceRepo/lab1_unix/data")
-nf<-read.table("Gesquiere2011_data.csv")
-head(nf)
+# load libraries below
 
+# Exercise 3 --------------------------------------------------------------
 
+### Question 1
 # load built-in dataset
 data(trees)
 # structure of a data frame
 str(trees)
 
 ### Using the tree data frame and calculate: what’s the average height of the cherry tree?
+#mean of Height in trees
 avgHeight = mean(trees$Height)
 print(avgHeight)
 #76
 
+### Question 2
 ###What is the average girth of those that are more than 75ft tall?
-over75 = trees$Girth[75 > trees$Girth]
-avgOver75 = mean(over75)  
+#subset data for trees over 75ft tall and get the girth
+over75 = trees$Girth[trees$Height > 75]
+#get mean girth
+avgOver75 = mean(over75)
 print(avgOver75)
-#13.24839
+#14.51176
 
+### Question 3
 ###What is the maximum height of trees with a volume between 15 and 35 ft^3?
-VolumeSet = trees$Volume[15 < trees$Volume < 35]
+#subset data for trees with volume between 15 and 35; get height of those trees
+VolumeSet = trees$Height[trees$Volume > 15 & trees$Volume < 35]
+#get max height 
+maxHeight = max(VolumeSet)
+print(maxHeight)
+#86 is the max height
 
-
-###Read in Read in “Gesquiere2011_data.csv”, for male ID equal 3, 
-#what’s the maximum GC value, what’s the mean T value?
-nf<-read.table("Gesquiere2011_data.csv", header = TRUE)
-
-
-###How many rows have GC larger than 50, and T larger than 100?
-
-# read.table separated the table using "tab" separator
-# but didn't interpret the first line as header
-nf<-read.table("Gesquiere2011_data.csv", header = TRUE)
-# read.csv can also be used
-# but need to specify the correct separator
-# default separator is comma
+### Question 4
+###Read in “Gesquiere2011_data.csv”,
+#for male ID equal 3, what’s the maximum GC value, what’s the mean T value?
+#read in csv
 nf<-read.csv("Gesquiere2011_data.csv", sep = '\t')
-nf<-read.csv("Gesquiere2011_data.csv", sep = ';')
-nf<-read.csv("Gesquiere2011_data.csv", skip = 5)
-
-##check
-write.csv(trees, "trees.csv")
-write.csv(trees,"trees.csv", append = TRUE)
-read.csv("trees.csv", row.names = TRUE)
-read.csv("trees.csv", col.names = FALSE)
-
-dim(nf)
 head(nf)
-tail(nf)
+
+#subset nf for male ID 3
+maleID_3 <- nf[nf$maleID == 3, ]
+print(maleID_3)
+
+#get maximum GC value
+max(maleID_3$GC)
+#136.31 maximum GC value for male ID 3
+
+#get mean T value
+mean(maleID_3$T)
+#149.0377 mean T value for male ID 3
 
 
+### Question 5
+###How many rows have GC larger than 50, and T larger than 100?
+#subset data for GC > 50 and T >100
+RowSubset <- nf[nf$GC > 50 & nf$T > 100, ]
+
+#count number of rows in subset
+numRows <- nrow(RowSubset)
+print(numRows)
+#1117 rows with GC larger than 50, and T larger than 100
